d0eb13cda4464d4f06747241707181baa3e4ce11
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2020-05-26 22:01:32 +0200

    Update mux4to1.v

--- mux4to1.v
+++ mux4to1.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 03/06/2020 08:57:55 PM
+// Create Date: 04/24/2020 02:00:05 PM
 // Design Name: 
-// Module Name: mux4to1
+// Module Name: mux_4to1
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,20 +20,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module mux4to1(
-    input [3:0] d,
-    input [1:0] s,
-    input [0:0] enbl,
-    output [0:0] z1
-    );
-    
-    not inst1 (net1, s[0]),
-inst2 (net2, s[1]);
-and inst3 (net3, d[0], net1, net2, enbl),
-inst4 (net4, d[1], s[0], net2, enbl),
-inst5 (net5, d[2], net1, s[1], enbl),
-inst6 (net6, d[3], s[0], s[1], enbl);
-or inst7 (z1, net3, net4, net5, net6);
-
-    
+module mux_4to1 (s0, s1, d0, d1, d2, d3, z1);
+//define inputs and output
+input s0, s1, d0, d1, d2, d3;
+output z1;
+//use the nested conditional operator
+assign z1 = s1 ? (s0 ? d3 : d2) : (s0 ? d1 : d0);
 endmodule
