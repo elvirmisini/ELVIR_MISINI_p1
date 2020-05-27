b9df1d19fbe3a15f4e6e3eb3553f454e25ba5776
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2020-05-27 15:53:50 +0200

    testbench shumzuesit 16bit(nevoja per ndryshime)

--- array_mu16_bip_tb.v
+++ array_mu16_bip_tb.v
@@ -1,16 +1,16 @@
-module array_mu3_bip_tb;
+module array_mu16_bip_tb;
 //inputs are reg for test bench
 //outputs are wire for test bench
-reg [2:0] a, b;
-wire [5:0] prod;
+  reg [15:0] a, b;
+  wire [31:0] prod;
 
 //apply stimulus and display variables
 initial
 begin: apply_stimulus
-reg [6:0] invect;
+  reg [32:0] invect;
 for (invect = 0; invect < 64; invect = invect + 1)
 begin
-{a, b} = invect [6:0];
+  {a, b} = invect [32:0];
 #10 $display ("a = %d, b = %d, prod = %d", a, b,
 prod);
 end
